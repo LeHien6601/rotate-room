1b16a55150975a29252200f59c329007adca6833
LeHien6601 <110620160+LeHien6601@users.noreply.github.com>
2024-06-02 06:53:36 +0700

    Add quit function

--- Rotate Room/Assets/Scripts/Canon.cs
+++ Rotate Room/Assets/Scripts/Canon.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
-
 public class Canon : MonoBehaviour
 {
     [SerializeField] private float duration;
